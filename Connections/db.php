bc0d0b79a6c0674b7fc19c9e131548900ab03398
Fabio <duarteleme@gmail.com>
2016-07-28 23:51:36 -0600

    bd e pastas ajustadas

--- Connections/db.php
+++ Connections/db.php
@@ -3,11 +3,8 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_db = "localhost";
-$database_db = "cinfer";
+$database_db = "banco_de_dados";
 $username_db = "root";
 $password_db = "";
 $db = mysql_pconnect($hostname_db, $username_db, $password_db) or trigger_error(mysql_error(),E_USER_ERROR)
-
 ?>
-
-
